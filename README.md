b730218ce5bd5c31780bacc961ecfb23bfdf16a0
kasztanka <m.lipowska@onet.pl>
2020-10-25 16:51:17 +0100

    Prepare experiments for a bigger number of workers.

--- README.md
+++ README.md
@@ -27,5 +27,13 @@ To run the experiments use the following commands:
 ```
 git clone https://github.com/kasztanka/federated-learning-mimiciii.git
 cd federated-learning-mimiciii
+python create_docker_compose_for_experiments.py
 docker-compose -f docker-compose.experiments.yml up
 ```
+This will create a docker-compose file and run it. By default the results will be saved in `./results/`.
+
+Additionally, you can pass a maximal number of workers used in experiments as a parameter to the Python script:
+```
+python create_docker_compose_for_experiments.py 3
+```
+The docker-compose.experiments.yml file in this repository is a sample file created by this script for three workers.
---
+++ create_docker_compose_for_experiments.py
@@ -0,0 +1,64 @@
+import os
+import sys
+
+NUM_OF_WORKERS = 128
+
+BEGINNING = '''version: "3"
+services:
+  network:
+    image: openmined/grid-network:production
+    environment:
+      - PORT=7000
+      - SECRET_KEY=ineedtoputasecrethere
+      - DATABASE_URL=sqlite:///databasenetwork.db
+    ports:
+      - 7000:7000
+
+'''
+
+WORKER_TEMPLATE = '''  worker{worker_id}:
+    image: openmined/grid-node:production
+    environment:
+      - NODE_ID=Worker{worker_id}
+      - ADDRESS=http://worker{worker_id}:{port}/
+      - PORT={port}
+      - NETWORK=http://network:7000
+      - DATABASE_URL=sqlite:///databasenode.db
+    depends_on:
+      - 'network'
+    ports:
+      - {port}:{port}
+
+'''
+
+END_TEMPLATE = '''  experiments:
+    depends_on:
+      - 'network'
+{workers}    build:
+      context: .
+      dockerfile: Experiments.Dockerfile
+    volumes:
+      - ./results:/results
+'''
+
+
+def main():
+    if len(sys.argv) > 1:
+        num_of_workers = int(sys.argv[1])
+        if num_of_workers > 2000:
+            print(
+                'Warning: Port of one worker may collide with the network port. Change this script or the network port')
+    else:
+        num_of_workers = NUM_OF_WORKERS
+
+    with open(f'docker-compose.experiments.yml', 'w') as f:
+        f.write(BEGINNING)
+        workers = ''
+        for i in range(num_of_workers):
+            f.write(WORKER_TEMPLATE.format(worker_id=i, port=5000 + i))
+            workers += '      - \'worker{}\''.format(i) + os.linesep
+        f.write(END_TEMPLATE.format(workers=workers))
+
+
+if __name__ == "__main__":
+    main()
